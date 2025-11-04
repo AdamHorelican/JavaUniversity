abc936d47c9f948a6236668aa5b0bc7104883fa0
Adam Horelican <horelicanadam@gmail.com>
2025-11-04 15:18:27 +0100

    error fixed

---
+++ exceptions/ErrorWithFile/src/ErrorWithJava.java
@@ -0,0 +1,20 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class ErrorWithJava
+{
+  public void openFile(String filename)
+  {
+    File file = new File(filename + ".txt");
+    try (Scanner scanner = new Scanner(file);)
+    {
+
+    }
+    catch (FileNotFoundException e){
+      System.out.println("Could not find file: " + file.getAbsolutePath());
+    }
+  }
+}
