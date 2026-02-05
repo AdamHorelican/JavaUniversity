9ad99927ed3b2136849c5d2820df39f0fdd6b310
Adam Horelican <horelicanadam@gmail.com>
2026-02-05 14:53:11 +0100

    using sychronized blocks

--- Threads/Threads2.2/src/Counter.java
+++ Threads/Threads2.2/src/Counter.java
@@ -6,10 +6,15 @@ public class Counter
     this.count = 0;
   }
 
-  public synchronized void incrementCount(){
-    count++;
+  public void incrementCount(){
+    synchronized (this){
+      count++;
+    }
+
   }
-  public synchronized int getCount(){
-    return count;
+  public int getCount(){
+    synchronized (this){
+      return count;
+    }
   }
 }
