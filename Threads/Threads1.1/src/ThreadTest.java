df2215f419cf92a2a44cee64f34cd057fc814f7a
Adam Horelican <horelicanadam@gmail.com>
2026-02-11 18:01:09 +0100

    Lambda expression

--- Threads/Threads1.1/src/ThreadTest.java
+++ Threads/Threads1.1/src/ThreadTest.java
@@ -9,9 +9,23 @@ public class ThreadTest
     //thread2.start();
 
 
-    WaitPrinter waitPrinter = new WaitPrinter(10);
-    Thread thread3 = new Thread(waitPrinter);
-    thread3.start();
+    //WaitPrinter waitPrinter = new WaitPrinter(10);
+    //Thread thread3 = new Thread(waitPrinter);
+    //thread3.start();
+
+    Thread waitLambda = new Thread(() -> {
+      for (int i = 0; i < 10; i++)
+      {
+        System.out.println(i);
+        try{
+          Thread.sleep(1000);
+        }
+        catch (InterruptedException e){
+          e.printStackTrace();
+        }
+      }
+    });
+    waitLambda.start();
 
   }
 }
